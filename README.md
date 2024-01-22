1e7674a76600e9612e16435420e906dbffc63c6b
aashutosh148 <aashutoshsingh468847@gmail.com>
2024-01-23 00:42:37 +0530

    bug fix multi-click issue

--- README.md
+++ README.md
@@ -6,18 +6,16 @@ YouTube Playlist Analytics is a powerful tool leveraging the Google YouTube API 
 
 ## Features
 
-- Retrieve detailed information about a YouTube playlist using the YouTube API v3.
-- Calculate the total combined length of all videos within the playlist.
-- Determine the average length of videos in the playlist.
-- Adjust video durations at different playback speeds (1.25x, 1.5x, 1.75x, 2.0x).
-- Display the results in a visually appealing and user-friendly format.
+-   Retrieve detailed information about a YouTube playlist using the YouTube API v3.
+-   Calculate the total combined length of all videos within the playlist.
+-   Determine the average length of videos in the playlist.
+-   Adjust video durations at different playback speeds (1.25x, 1.5x, 1.75x, 2.0x).
+-   Display the results in a visually appealing and user-friendly format.
 
 ## Requirements
 
-- Vanilla JavaScript
-- YouTube API v3 key (Acquire it from the [Google Developer Console](https://console.developers.google.com/))
-
-
+-   Vanilla JavaScript
+-   YouTube API v3 key (Acquire it from the [Google Developer Console](https://console.developers.google.com/))
 
 ## Usage
 
@@ -31,7 +29,6 @@ This project is licensed under the [MIT License](LICENSE).
 
 ## Acknowledgments
 
-- [Google YouTube API v3 Documentation](https://developers.google.com/youtube/v3)
-
+-   [Google YouTube API v3 Documentation](https://developers.google.com/youtube/v3)
 
 Feel free to contribute, report issues, or provide feedback!
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let playlistId = '';
 document.write('<script type="text/javascript" src="assets/js/apiKey.js"></script>');
 document.write('<script type="text/javascript" src="assets/js/videoUtils.js"></script>');
 
-// Initialize counters and variables
 let totalVideos = 0;
 let nextPageToken = null;
 const allVideoId = [];
@@ -40,50 +39,79 @@ function toggleDarkMode() {
 }
 
 
-
-// Execute the async function
 document.getElementById('calculate').addEventListener('click', async function () {
+    calculatePlaylistStats();
+});
+
+document.getElementById('videoUrl').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        calculatePlaylistStats();
+    }
+});
+
+function formatResult(duration) {
+    return `${duration.days > 0 ? `${duration.days} days, ` : ''}${duration.hours > 0 ? `${duration.hours} hours, ` : ''}${duration.minutes > 0 ? `${duration.minutes} minutes, ` : ''}${duration.seconds > 0 ? `${duration.seconds} seconds` : ''}`;
+}
+
+async function calculatePlaylistStats() {
     playlistId = extractPlaylistId();
     await fetchVideos();
+    document.getElementById('result').innerHTML = '';
 
     const { totalVideos, totalDurationInSeconds } = await fetchVideoDurations(allVideoId);
 
     const formattedDuration = formatDuration(totalDurationInSeconds);
-    
 
     const speed1_25x = 1.25;
     const speed1_5x = 1.5;
     const speed1_75x = 1.75;
     const speed2x = 2.0;
 
-    const avg = formatDuration(totalDurationInSeconds/totalVideos);
+    const avg = formatDuration(totalDurationInSeconds / totalVideos);
     const adjusted1_25x = calculateAdjustedDuration(totalDurationInSeconds, speed1_25x);
     const adjusted1_5x = calculateAdjustedDuration(totalDurationInSeconds, speed1_5x);
     const adjusted1_75x = calculateAdjustedDuration(totalDurationInSeconds, speed1_75x);
     const adjusted2x = calculateAdjustedDuration(totalDurationInSeconds, speed2x);
 
 
-    function formatResult(duration) {
-        return `${duration.days > 0 ? `${duration.days} days, ` : ''}${duration.hours > 0 ? `${duration.hours} hours, ` : ''}${duration.minutes > 0 ? `${duration.minutes} minutes, ` : ''}${duration.seconds > 0 ? `${duration.seconds} seconds` : ''}`;
+    const formattedAvg = formatResult(avg);
+    const formattedAdjusted1_25x = formatResult(adjusted1_25x);
+    const formattedAdjusted1_5x = formatResult(adjusted1_5x);
+    const formattedAdjusted1_75x = formatResult(adjusted1_75x);
+    const formattedAdjusted2x = formatResult(adjusted2x);
+
+
+    const statsData = {
+        totalVideos,
+        avg: formattedAvg,
+        formattedDuration: formatResult(formattedDuration),
+        adjusted1_25x: formattedAdjusted1_25x,
+        adjusted1_5x: formattedAdjusted1_5x,
+        adjusted1_75x: formattedAdjusted1_75x,
+        adjusted2x: formattedAdjusted2x
+    };
+
+    localStorage.setItem('playlistStats', JSON.stringify(statsData));
+
+    location.reload();
+}
+
+function displayStoredPlaylistStats() {
+    const storedStatsData = localStorage.getItem('playlistStats');
+
+    if (storedStatsData) {
+        const statsData = JSON.parse(storedStatsData);
+
+        document.getElementById('result').innerHTML = `
+            <p>No of videos : ${statsData.totalVideos}</p>
+            <p>Average length of video : ${statsData.avg}</p>
+            <p>Total length of playlist : ${statsData.formattedDuration}</p>
+            <p>At 1.25x : ${statsData.adjusted1_25x}</p>
+            <p>At 1.50x : ${statsData.adjusted1_5x}</p>
+            <p>At 1.75x : ${statsData.adjusted1_75x}</p>
+            <p>At 2.00x : ${statsData.adjusted2x}</p>
+        `;
     }
+}
 
-    // Display the results on the webpage
-    document.getElementById('result').innerHTML = `
-        <p>No of videos : ${totalVideos}</p>
-        <p>Average length of video : ${formatResult(avg)}</p>
-        <p>Total length of playlist : ${formatResult(formattedDuration)}</p>
-        <p>At 1.25x : ${formatResult(adjusted1_25x)}</p>
-        <p>At 1.50x : ${formatResult(adjusted1_5x)}</p>
-        <p>At 1.75x : ${formatResult(adjusted1_75x)}</p>
-        <p>At 2.00x : ${formatResult(adjusted2x)}</p>
-    `;
-
-
-
-});
-
-
-
-
-
-
+window.onload = displayStoredPlaylistStats;
